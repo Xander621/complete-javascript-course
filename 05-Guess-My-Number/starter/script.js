4047256492b0c2a7f788251a345853be014c7676
Alex Lance <alexander.lance@gmail.com>
2021-11-03 08:45:48 -0700

    Add challenge #1 code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,54 +1,79 @@
 'use strict';
 
-// // This is a read from the HTML document message class
-// console.log(document.querySelector('.message').textContent);
+// Default values
+const defaultScore = '20';
+const defaultBackgroundColor = '#222';
+const defaultNumberBoxWidth = '15rem';
+let highScore = '0';
 
-// // Here we are manipulating the DOM node for message by setting a new string
-// document.querySelector('.message').textContent = 'Correct Number!';
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = Number(document.querySelector('.score').textContent);
 
-// // If we log from again we'll see the new string
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
-// When we click on the 'Check' button element grab the value
-// from guess and log it.
-// document.querySelector('.check').addEventListener('click', function () {
-//   console.log(`Value is ${document.querySelector('.guess').value}`);
-// });
-
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-let count = Number(document.querySelector('.score').textContent);
-
-console.log(`${secretNumber}`);
-console.log(`Score: ${count}`);
+// console.log(`${secretNumber}`);
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(`Guess is of type ${typeof guess} and the value is ${guess}`);
 
-  if (count > 0) {
+  if (score > 1) {
     if (!guess) {
       document.querySelector('.message').textContent = 'No Number!';
     } else if (guess < secretNumber) {
-      count--;
-      document.querySelector('.score').textContent = `${count}`;
-      document.querySelector('.message').textContent = 'Guess to Low!';
+      score--;
+      document.querySelector('.score').textContent = `${score}`;
+      document.querySelector('.message').textContent = 'To Low!';
     } else if (guess > secretNumber) {
-      count--;
-      document.querySelector('.score').textContent = `${count}`;
-      document.querySelector('.message').textContent = 'Guess to High!';
+      score--;
+      document.querySelector('.score').textContent = `${score}`;
+      document.querySelector('.message').textContent = 'To High!';
     } else {
-      document.querySelector('.message').textContent = `Guess correct!`;
-      document.querySelector('.score').textContent = `${count}`;
-      document.querySelector('.highscore').textContent = `${count}`;
-      console.log(`Score is ${count}`);
+      // Change the background color using the DOM
+      document.body.style.backgroundColor = '#60b347';
+      //   document.querySelector('body').style.backgroundColor = '#60b347';
+
+      // Change the width of the number field using the DOM
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = `${guess}`;
+
+      document.querySelector('.message').textContent = `Correct Number!`;
+      document.querySelector('.score').textContent = `${score}`;
+      document.querySelector('.highscore').textContent = `${score}`;
+      //   console.log(`Score is ${score}`);
+      if (Number(highScore) == 0 || Number(highScore) < Number(score)) {
+        highScore = `${score}`;
+      }
     }
   } else {
-    document.querySelector('.message').textContent = `You Lose.`;
+    score--;
+    document.querySelector('.score').textContent = `${score}`;
+    document.querySelector('.message').textContent = `You lost this round!`;
   }
+});
+
+/**
+ * Coding Challenge #1
+ *
+ * Implement a game reset functionality, so that the player can make a new guess.
+ * Here is how:
+ * 1. Select the element with the 'again' class and attach a click event
+ *    handler.
+ * 2. in the handler function, restore the inital values of the score and
+ *    number variables
+ * 3. Restore the initial conditions of the message, number and number variables
+ * 4. Also restore the original background color (#222) and number width (15rem)
+ */
+document.querySelector('.again').addEventListener('click', () => {
+  // Reset initial game values
+  document.body.style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = `${defaultNumberBoxWidth}`;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = `${defaultScore}`;
+  document.querySelector('.highscore').textContent = `${highScore}`;
+  // Treating score as an integer so we're setting it explictly
+  score = Number(defaultScore);
+
+  // Generate a new number
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
 });
--- my-dev-notes/my-notes.md
+++ my-dev-notes/my-notes.md
@@ -3,13 +3,14 @@
 * Prettier
 * Live Server -or- npm package
 
-## Using Live Server
-
 ## Open Terminal in VSCode
 * RCTRL+RSHIFT+`
 
 ## Install Live Server using NPM
 npm install live-server -g
 
+## Section 07 - JavaScript in the Browser: DOM and Events Fundamentals
 
-
+Document Object Model: Structured representation of HTML Documents allows JavaScript to 
+access HTML elements and styles to manipulate them.
+* A connection point between HTML documents and Javascript code.
