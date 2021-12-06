480d82432543cecd57cb6934b1b661896f0ad0f2
Alex Lance <alexander.lance@gmail.com>
2021-12-06 09:01:31 -0700

    chore: add section 10-130 coursework

--- 10-Functions/starter/coursework.js
+++ 10-Functions/starter/coursework.js
@@ -41,35 +41,82 @@
  * [SECTION 10-129] How Passing Arguments Works: Value vs. Reference
  * Javascript does not pass by reference, only by value.
  ******************************************************************************/
-const flight = 'LH234';
-const passenger = {
-  name: 'Alex Lance',
-  passport: 24739479284,
-}
+// const flight = 'LH234';
+// const passenger = {
+//   name: 'Alex Lance',
+//   passport: 24739479284,
+// }
 
-const checkIn = function(flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function(flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if(passenger.passport === 24739479284) {
-    alert('Check in')
-  } else {
-    alert('Wrong passport!')
-  }
-};
+//   if(passenger.passport === 24739479284) {
+//     alert('Check in')
+//   } else {
+//     alert('Wrong passport!')
+//   }
+// };
 
+// // checkIn(flight, passenger);
+// // console.log('flight :>> ', flight);
+// // console.log('passenger :>> ', passenger);
+
+// const newPassport = function(person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// }
+
+// newPassport(passenger);
 // checkIn(flight, passenger);
-// console.log('flight :>> ', flight);
-// console.log('passenger :>> ', passenger);
-
-const newPassport = function(person) {
-  person.passport = Math.trunc(Math.random() * 100000000000);
-}
-
-newPassport(passenger);
-checkIn(flight, passenger);
 
 /***********************[END OF 10-129]**************************************/
+
+ /******************************************************************************
+ * [SECTION 10-130] First-Class and Higher-Order Functions
+ ******************************************************************************/
+  /* FIRST-CLASS FUNCTIONS
+  * * Javascript treats functions as first-class citizens, i.e.
+  * * this means that functions are treated simply as values
+  * * Functions are just another 'type' of object
+  * * Store functions in variables or properties:
+  * const add = (a,b) => a + b;
+  * const counter = {
+  *   value: 23,
+  *   inc: function() {this.value++;}
+  * }
+  *
+  * * Pass functions as arguments to OTHER functions:
+  * const greet = () => console.log('Hello');
+  * btnClose.addEventListener('click', greet);
+  *
+  * * Return fucntions FROM functions
+  * 
+  * * Call methods on functions:
+  * counter.inc.bind(someOtherObject);
+  */
+
+  /* HIGHER-ORDER FUNCTIONS
+   * * A Function that receives another function as an argument, that returns a
+   *   new function, or both
+   * * This is only possible because of first-class functions
+   * 
+   * 1. Function that receives another function
+   * const greet = () => console.log('Hello');
+   * // addEventListener function is the higher-order function
+   * // greet is a callback function
+   * btnClose.addEventListener('click', greet);  
+   * 
+   * 2. Functions that return new function
+   * // count is th higher-order function
+   * function count() {
+   *  let counter = 0;
+   *  return function() {
+   *    counter++; 
+   *  };
+   * }
+
+ /***********************[END OF 10-130]**************************************/
+
 
  /******************************************************************************
  * [SECTION 10-1##]
