ab674b90fe5f90eab2dbf0866c199a2e90643775
alance <alance@brewmaster.lab>
2022-10-07 08:11:41 -0600

    chore: add Section 13: 198 coursework

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -344,7 +344,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function(entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if(!entry.isIntersecting)
     nav.classList.add('sticky');
   else
@@ -354,3 +354,28 @@ const stickyNav = function(entries) {
 
 const headerObserver = new IntersectionObserver(stickyNav, {root: null, threshold: 0, rootMargin: `-${navHeight}px`});
 headerObserver.observe(header);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Section 13 - 198 Revealing Elements on Scroll
+ */
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section')
+
+const revealSection = function(entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if(!entry.isIntersecting) return;   // guard clause
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {root: null, threshold: 0.15});
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
